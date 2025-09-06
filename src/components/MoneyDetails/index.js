a37d7d2862d6c85165f80d83cae28d9cafc4cd7c
Prem Kumar <113364219+premkumarthouda@users.noreply.github.com>
2025-09-06 17:18:38 +0530

    Update index.js

--- src/components/MoneyDetails/index.js
+++ src/components/MoneyDetails/index.js
@@ -1,7 +1,8 @@
 // Write your code here
 import './index.css'
 
-const MoneyDetails = () => {
+const MoneyDetails = props => {
+  const {BALANCE, INCOME, EXPENSES} = props
   return (
     <div className="card-container">
       <div className="balance-container">
@@ -12,29 +13,35 @@ const MoneyDetails = () => {
         />
         <div className="ruppes">
           <p className="user-balance">Your Balance</p>
-          <p className="paragraph">RS 0</p>
+          <p className="paragraph" data-testid="balanceAmount">
+            RS {BALANCE}
+          </p>
         </div>
       </div>
       <div className="Income-container">
         <img
-          src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png"
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png"
           className="price-img"
-          alt="balance"
+          alt="income"
         />
         <div className="ruppes">
           <p className="user-balance">Your Income</p>
-          <p className="paragraph">RS 0</p>
+          <p className="paragraph" data-testid="incomeAmount">
+            RS {INCOME}
+          </p>
         </div>
       </div>
       <div className="Expenses-container">
         <img
-          src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png"
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png "
           className="price-img"
-          alt="balance"
+          alt="expenses"
         />
         <div className="ruppes">
           <p className="user-balance">Your Expenses</p>
-          <p className="paragraph">RS 0</p>
+          <p className="paragraph" data-testid="expensesAmount">
+            RS {EXPENSES}
+          </p>
         </div>
       </div>
     </div>
